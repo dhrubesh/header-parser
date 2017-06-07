31545ebdf1fde358059118cf6f1cb4fedc744010
dhrubesh <dhrubesh97@gmail.com>
2017-06-07 19:19:28 +0000

    language functionality

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+var API = '/api/whoami'
+app.use(morgan('dev'))
 
-app.get('/', function (req, res) {
+
+
+app.get(API, function (req, res) {
+    console.log(req)
+    var language = req.headers['accept-language']
+    console.log(language)
   res.send('Hello World!')
 })
 
